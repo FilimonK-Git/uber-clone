3b600a1ed9c18ba0265b52530030b321da776375
Filimon Kiros <filimona.kiros@gmail.com>
2023-03-19 16:22:38 -0400

    google places autocomplete added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GoogleMapsAPI } from "@env";
 
 const HomeScreen = () => {
   return (
@@ -16,6 +18,26 @@ const HomeScreen = () => {
           source={{
             uri: "https://links.papareact.com/gzs",
           }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="From where?"
+          debounce={400}
+          onPress={(data, details = null) => {}}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          query={{
+            key: GoogleMapsAPI,
+            language: "en",
+          }}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 20,
+            },
+          }}
+          enablePoweredByContainer={false}
         />
         <NavOptions />
       </View>
